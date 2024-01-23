fcb14fff4d5f263c2c590f60cca657d785f84ac1
Ryszard Schossler <ryszard.schossler@gmail.com>
2024-01-23 09:30:28 +0100

    Add models

---
+++ RickAndMorty/Models/RMCharacterGender.swift
@@ -0,0 +1,15 @@
+//
+//  RMCharacterGender.swift
+//  RickAndMorty
+//
+//  Created by Ryszard Schossler on 23/01/2024.
+//
+
+import Foundation
+
+enum RMCharacterGender: String, Codable {
+    case female = "Female"
+    case male = "Male"
+    case genderless = "Genderless"
+    case `unknown` = "unknown"
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Ryszard Schossler on 23/01/2024.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+}
